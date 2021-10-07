e1a38e2d99909547c904bcd8b6c0cdb22c25e266
Abhishek <67120792+abhishekgarg1489@users.noreply.github.com>
2021-10-07 15:29:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+var requests =  require("requests");
+
+const homeFile = fs.readFileSync("home.html",'utf-8');
+
+const replaceVal = (tempVal,orgVal)=>{
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+    return temperature;
+}
+
+const server = http.createServer((req,res)=>{
+    if(req.url='/'){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Kota&units=metric&appid=ee5544aabff74c70b1cf5cc7b2cfcbab")
+        .on('data',(chunk)=>{
+            const objData = JSON.parse(chunk);
+            const arrData = [objData];
+            //console.log(arrData[0].main.temp);
+            const realTimeData = arrData.map(val=>replaceVal(homeFile,val)).join("");
+            console.log(realTimeData);
+            res.write(realTimeData);
+        })
+        .on('end',(err)=>{
+            if(err)return console.log('connection closed due to errors ',err);
+            res.end();
+        });
+    }
+});
+
+server.listen(8000,"127.0.0.1");
